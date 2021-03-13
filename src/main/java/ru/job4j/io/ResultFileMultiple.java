3373348358d0778f6e6b16f652f642550fb96ffe
Mosquitod <Mosquitod3@yandex.ru>
2021-03-13 19:05:10 +0300

    FileOutputStream. [#457734]

---
+++ src/main/java/ru/job4j/io/ResultFileMultiple.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFileMultiple {
+
+    public void inputInFile(int size) {
+        int[][] table = multiple(size);
+        try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
+            for (int[] row : table) {
+                for (int cell : row) {
+                    out.write(String.valueOf(cell).getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                table[i - 1][j - 1] = i * j;
+            }
+        }
+        return table;
+    }
+}
