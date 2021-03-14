58293aac8c662ae6dfe8454df7c030d49aae5127
Mosquitod <Mosquitod3@yandex.ru>
2021-03-14 19:21:26 +0300

    BufferedOutputStream [#457736]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,16 +1,12 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     public static List<String> filter(String file) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("log.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
           return reader.lines().filter(el -> el.contains("404")).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
@@ -18,8 +14,15 @@ public class LogFilter {
         return null;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.stream().forEach(el -> out.printf("%s%n", el));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(filter("log.txt"), "logFilter.txt");
     }
 }
