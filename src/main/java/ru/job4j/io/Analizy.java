41de1398c37828f0f7c3206faf77ddaa110d0202
Mosquitod <Mosquitod3@yandex.ru>
2021-03-24 22:05:24 +0300

    Анализ доступности сервера. refactoring

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,32 +1,32 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analizy {
+    private boolean mark = true;
+    private StringBuilder builder = new StringBuilder();
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            List<String> line = reader.lines().filter(el -> !el.isEmpty()).collect(Collectors.toList());
-            for (int index = 0; index < line.size(); index++) {
-                String txt;
-                if (line.get(index).contains("400") || line.get(index).contains("500")) {
-                    txt = line.get(index).substring(line.get(index).indexOf(" ") + 1) + ";";
-                    int nextIndex = index + 1;
-                    if (line.get(nextIndex).contains("200") || line.get(nextIndex).contains("300")) {
-                        txt += line.get(nextIndex).substring(line.get(nextIndex).indexOf(" ") + 1);
-                    }
-                    writeToFile(target, txt);
+            reader.lines().forEach(str -> {
+                int index = str.indexOf(" ") + 1;
+                if (mark && str.contains("400") || str.contains("500")) {
+                    builder.append(str.substring(index) + ";");
+                    mark = false;
                 }
-            }
+                if (!mark && str.contains("200") || str.contains("300")) {
+                    builder.append(str.substring(index) + System.lineSeparator());
+                    mark = true;
+                }
+            });
+            writeToFile(target, builder.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void writeToFile(String target, String stringDate) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target, true))) {
-            out.printf("%s%n", stringDate);
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            out.print(stringDate);
         } catch (Exception e) {
             e.printStackTrace();
         }
