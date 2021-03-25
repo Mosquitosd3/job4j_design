c9f15964bb21c83d5d55b1759fd51343a189a102
Mosquitod <Mosquitod3@yandex.ru>
2021-03-25 22:02:38 +0300

    Анализ доступности сервера. refactoring v2

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,31 +1,36 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizy {
     private boolean mark = true;
-    private StringBuilder builder = new StringBuilder();
+    private List<String> result = new ArrayList<>();
+    private int nextLine = 0;
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-            reader.lines().forEach(str -> {
+            reader.lines().filter(str -> !str.isEmpty()).forEach(str -> {
                 int index = str.indexOf(" ") + 1;
                 if (mark && str.contains("400") || str.contains("500")) {
-                    builder.append(str.substring(index) + ";");
+                    result.add(str.substring(index) + ";");
                     mark = false;
                 }
                 if (!mark && str.contains("200") || str.contains("300")) {
-                    builder.append(str.substring(index) + System.lineSeparator());
+                    result.add(str.substring(index) + System.lineSeparator());
                     mark = true;
                 }
+                if (nextLine < result.size()) {
+                    writeToFile(target, result.get(nextLine++));
+                }
             });
-            writeToFile(target, builder.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void writeToFile(String target, String stringDate) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target, true))) {
             out.print(stringDate);
         } catch (Exception e) {
             e.printStackTrace();
