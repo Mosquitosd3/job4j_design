e606e14ebc18c165922bbb67d8502fd32878ff28
Mosquitod <Mosquitod3@yandex.ru>
2021-03-26 18:48:17 +0300

    Тестирование IO

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,15 +1,18 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void whenUnavailable() throws IOException {
         String source = "./files/server.log";
@@ -22,5 +25,22 @@ public class AnalizyTest {
             reader.lines().forEach(builder::append);
         }
         assertThat(expect, is(builder.toString()));
+    }
+
+    @Test
+    public void whenTemporaryFolder() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        Analizy analizy = new Analizy();
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(source)));
+             BufferedReader in = new BufferedReader(new FileReader("./files/server.log"))) {
+            in.lines().filter(str -> !str.isEmpty()).forEach(line -> out.println(line));
+        }
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:58:01;10:59:0111:01:02;11:02:02"));
     }
 }
