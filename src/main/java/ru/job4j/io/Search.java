43e263bb6bb02babbf0cae77e900b3b3f71ed474
Mosquitod <Mosquitod3@yandex.ru>
2021-04-01 16:52:24 +0300

    Валидация параметров запуска. refactoring Validation

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length > 2) {
+        if (args.length == 0) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
         Path start = Paths.get(args[0]);
