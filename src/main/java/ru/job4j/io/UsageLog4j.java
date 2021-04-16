c3e58b59914aeec1002922d47540f763c4bb433d
Mosquitod <Mosquitod3@yandex.ru>
2021-04-16 18:47:19 +0300

    Slf4j - вывод переменных.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -14,5 +14,19 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        byte b = Byte.MAX_VALUE;
+        short sh = 32000;
+        int i = 10;
+        long l = 100L;
+        float f = 1.0F;
+        double d = 10.0D;
+        char c = 'A';
+        boolean is = true;
+
+        LOG.debug("byte {} short {} int {} long {} float {} double {} char {} boolean {}", b, sh, i, l, f, d, c, is);
+
+
+
     }
 }
