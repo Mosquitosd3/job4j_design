06462b073901bd6840ea3bc2b843d3ac9f8f2163
Mosquitod <Mosquitod3@yandex.ru>
2021-05-10 21:18:27 +0300

    Подключение к БД

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,26 @@
+package ru.job4j.jdbc;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
+      Class.forName("org.postgresql.Driver");
+      Properties props = new Properties();
+      ClassLoader loader = ConnectionDemo.class.getClassLoader();
+
+      props.load(loader.getResourceAsStream("app.properties"));
+      String url = props.getProperty("url");
+      String login = props.getProperty("login");
+      String password = props.getProperty("password");
+      try (Connection connection = DriverManager.getConnection(url, login, password)){
+          DatabaseMetaData metaData = connection.getMetaData();
+          System.out.println(metaData.getURL());
+          System.out.println(metaData.getUserName());
+      }
+    }
+}
