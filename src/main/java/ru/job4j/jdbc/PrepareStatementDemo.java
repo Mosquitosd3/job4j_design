fbe8d88913cfe43191be25d0a83623eadfef0f90
Mosquitod <Mosquitod3@yandex.ru>
2021-05-13 23:58:53 +0300

    PrepareStatement

---
+++ src/main/java/ru/job4j/jdbc/PrepareStatementDemo.java
@@ -0,0 +1,109 @@
+package ru.job4j.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PrepareStatementDemo {
+    private Connection connection;
+
+    public PrepareStatementDemo() {
+        connectionInit();
+    }
+
+    public void createTable() {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "create table if not exists cities(%s, %s, %s);",
+                    "id serial primary key",
+                    "name text",
+                    "population int"
+            );
+            statement.execute(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public List<City> findAll() {
+        List<City> result = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("select * from cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    result.add(new City(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getInt("population")
+                    ));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
+
+    public City insert(City city) {
+        try (PreparedStatement statement = connection.prepareStatement("insert into cities(name, population) values(?, ?)", Statement.RETURN_GENERATED_KEYS)){
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getId());
+            statement.execute();
+            try (ResultSet generateKeys = statement.getGeneratedKeys()) {
+                city.setId(generateKeys.getInt(1));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return city;
+    }
+
+    public boolean update(City city) {
+        boolean result = false;
+        try (PreparedStatement statement = connection.prepareStatement("update cities set name = ?, population = ? where id = ?")){
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.setInt(3, city.getId());
+            result = statement.executeUpdate() > 0;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean delete(int id) {
+        boolean result = false;
+        try (PreparedStatement statement = connection.prepareStatement("delete from cities where id = ?") ){
+            statement.setInt(1, id);
+            result = statement.executeUpdate() > 0;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
+    }
+
+    private void connectionInit() {
+        Properties properties = new Properties();
+        ClassLoader loader = this.getClass().getClassLoader();
+        try (InputStream io = loader.getResourceAsStream("app.properties")){
+            properties.load(io);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            Class.forName(properties.getProperty("driver"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        try {
+            connection = DriverManager.getConnection(url, login, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
